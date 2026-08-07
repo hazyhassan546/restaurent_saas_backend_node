9fb98a312312f9ebebaa8fcaa407f5ecc742b049
Hassan <hazyhassan546@gmail.com>
2026-08-07 22:21:44 +0500

    fixes

--- src/controllers/auth/authController.ts
+++ src/controllers/auth/authController.ts
@@ -3,9 +3,10 @@ import {
   generateRefreshToken,
 } from "../../services/jwtService";
 import { sendOTP, validateOTP } from "../../services/otpService";
-import { isValidOtpFormat, isValidPhone } from "../../utils/helperFunctions";
+import { isValidPhone } from "../../utils/helperFunctions";
 import prisma from "../../utils/prisma";
 import { getUserByPhone } from "../users/userController";
+import jwt from "jsonwebtoken";
 
 export const initializeUserLogin = async (req: any, res: any) => {
   try {
@@ -69,5 +70,49 @@ export const verifyOTP = async (req: any, res: any) => {
   } catch (error) {
     console.error("Error logging in user:", error);
     return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const refreshToken = async (req: any, res: any) => {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader?.startsWith("Bearer ")) {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
+    }
+    const old_token = authHeader.split(" ")[1];
+    const decoded: any = jwt.verify(
+      old_token,
+      process.env.JWT_REFRESH_SECRET as string,
+    );
+    const userId = decoded.id;
+
+    const user = await prisma.users.findUnique({
+      where: { id: userId },
+    });
+
+    if (!user) {
+      throw new Error("Invalid or expired token");
+    }
+
+    // Generate JWT
+    const token = generateAuthToken(user);
+    const refresh_token = generateRefreshToken(user);
+
+    res.send({
+      message: "Login successful",
+      user: {
+        id: user.id,
+        name: user.full_name,
+        phone: user.phone,
+      },
+      access_token: token,
+      refresh_token: refresh_token,
+    });
+  } catch (error) {
+    return res.status(401).json({
+      message: "Invalid or expired token",
+    });
   }
 };
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   initializeUserLogin,
+  refreshToken,
   verifyOTP,
 } from "../controllers/auth/authController";
 
@@ -8,5 +9,6 @@ const router = Router();
 
 router.post("/init-login", initializeUserLogin);
 router.post("/verify-otp", verifyOTP);
+router.post("/refresh-token", refreshToken);
 
 export default router;
